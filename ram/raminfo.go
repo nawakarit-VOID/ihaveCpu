e23d6948865d9428d84126815bb95f9359598d10
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-06-18 09:37:52 +0700

    #new_processValue

--- ram/raminfo.go
+++ ram/raminfo.go
@@ -24,15 +24,102 @@ func Memory() *ghw.MemoryInfo {
 }
 
 /*
-func g() *ghw.GPUInfo {
-	gpuInfo, err := ghw.GPU()
-	if err != nil {
-		return nil
-	}
-	return gpuInfo
+	func g() *ghw.GPUInfo {
+		gpuInfo, err := ghw.GPU()
+		if err != nil {
+			return nil
+		}
+		return gpuInfo
 
 }
 */
+func processValue(value float64) (float64, string) {
+	// ตัวอักษร flag ที่สัมผัส
+	var x string = ""
+	// ตรวจสอบเงื่อนไข //แบบ บนลงล่าง
+	switch {
+	case value >= 1099511627776:
+		value = value / 1099511627776
+		x = "TB"
+	case value >= 1073741824:
+		value = value / 1073741824
+		x = "GB"
+	case value >= 1048576:
+		value = value / 1048576
+		x = "MB"
+	case value >= 1000:
+		value = value / 1024
+		x = "KB"
+	default:
+		x = "B"
+	}
+	return value, x
+}
+
+func newProcessValue(value float64) (float64, string) {
+	// ตัวอักษร flag ที่สัมผัส
+	var x string = "B" //8Bit = 1Byte
+	// ตรวจสอบเงื่อนไข //แบบ บนลงล่าง
+	if value > 1024 {
+		value = value / 1024
+		x = "KB"
+		if value > 1024 {
+			value = value / 1024
+			x = "MB"
+			if value > 1024 {
+				value = value / 1024
+				x = "GB"
+				if value > 1024 {
+					value = value / 1024
+					x = "TB"
+					if value > 1024 {
+						value = value / 1024
+						x = "PB"
+						if value > 1024 {
+							value = value / 1024
+							x = "EB"
+							if value > 1024 {
+								value = value / 1024
+								x = "ZB"
+								if value > 1024 {
+									value = value / 1024
+									x = "YB"
+									if value > 1024 {
+										value = value / 1024
+										x = "Bronto Byte"
+										if value > 1024 {
+											value = value / 1024
+											x = "Geop Byte"
+
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/*
+	   case value >= 1099511627776:
+	   		value = value / 1099511627776
+	   		x = "TB"
+	   	case value >= 1073741824:
+	   		value = value / 1073741824
+	   		x = "GB"
+	   	case value >= 1048576:
+	   		value = value / 1048576
+	   		x = "MB"
+	   	case value >= 1000:
+	   		value = value / 1024
+	   		x = "KB"
+	   	default:
+	   		x = "B"
+	   	}*/
+	return value, x
+}
 
 // dmidecode
 func RamTabs() fyne.CanvasObject {
@@ -41,27 +128,44 @@ func RamTabs() fyne.CanvasObject {
 
 	info += fmt.Sprintf("Area: %v\n", memInfo.Area)
 
+	q1, q2 := newProcessValue(float64(memInfo.TotalPhysicalBytes))
+
+	info += fmt.Sprintf("newProcessValue : %.2f  %s\n", q1, q2)
+
 	x1 := float64(memInfo.TotalPhysicalBytes)
 	x2 := float64(x1) / 1024
 	x3 := float64(x2) / 1024
 	x4 := float64(x3) / 1024
+
+	a, a1 := processValue(x1)
+
 	//info += fmt.Sprintf("TotalPhysicalBytes: %d\n", memInfo.TotalPhysicalBytes)
 	info += fmt.Sprintf("TotalPhysicalBytes: %.2f byte\n", x1)
 	info += fmt.Sprintf("TotalPhysicalBytes: %.2f Kb\n", x2)
 	info += fmt.Sprintf("TotalPhysicalBytes: %.2f Mb\n", x3)
 	info += fmt.Sprintf("TotalPhysicalBytes: %.2f Gb\n", x4)
 
-	xx1 := float64(memInfo.TotalUsableBytes)
-	xx2 := float64(xx1) / 1024
-	xx3 := float64(xx2) / 1024
-	xx4 := float64(xx3) / 1024
+	info += fmt.Sprintf("processValue: %.2f %s\n", a, a1)
+
+	xx1 := float64(memInfo.TotalUsableBytes) //b
+	xx2 := float64(xx1) / 1024               //Kb
+	xx3 := float64(xx2) / 1024               //Mb
+	xx4 := float64(xx3) / 1024               //Gb
+
+	b, b1 := processValue(x1)
+
 	//info += fmt.Sprintf("TotalUsableBytes: %d\n", memInfo.TotalUsableBytes)
 	info += fmt.Sprintf("TotalPhysicalBytes: %.2f byte\n", xx1)
 	info += fmt.Sprintf("TotalPhysicalBytes: %.2f Kb\n", xx2)
 	info += fmt.Sprintf("TotalPhysicalBytes: %.2f Mb\n", xx3)
-	info += fmt.Sprintf("TotalPhysicalBytes: %.2f Gb\n", xx4)
+	info += fmt.Sprintf("TotalPhysicalBytes: %.2f Gb\n", xx4) //มีจุดทศนิยม
+	info += fmt.Sprintf("processValue: %.2f %s\n", b, b1)     //ถูกปัดเศษขึ้น
 
-	info += fmt.Sprintf("DefaultHugePageSize: %d\n", memInfo.DefaultHugePageSize)
+	xz1 := float64(memInfo.DefaultHugePageSize)
+	xz2 := float64(xz1) / 1024
+	xz3 := float64(xz2) / 1024
+	//info += fmt.Sprintf("DefaultHugePageSize: %d\n", memInfo.DefaultHugePageSize)
+	info += fmt.Sprintf("DefaultHugePageSize: %.2f Mb\n", xz3)
 
 	for i, m := range memInfo.Modules {
 		info += fmt.Sprintf("\nModule %d\n", i+1)
@@ -71,19 +175,37 @@ func RamTabs() fyne.CanvasObject {
 		info += fmt.Sprintf("  Serial : %s\n", m.SerialNumber)
 		info += fmt.Sprintf("  Serial : %s\n", m.Label)
 		info += fmt.Sprintf("  Serial : %s\n", m.Location)
-
-	}
+	} //*ไม่ขึ้น
 
 	for size, amount := range memInfo.HugePageAmountsBySize {
-		info += fmt.Sprintf("HugePage %d : %d\n", size, amount)
+		var xx1 float64
+		var xx2 float64
+		var xx3 float64
+		var xx4 float64
+
+		xx1 = float64(size)
+		if xx1 > 1024 {
+			xx2 = float64(xx1) / 1024 //Kb
+		}
+		if xx2 > 1024 {
+			xx3 = float64(xx1) / 1024 //Mb
+		}
+		if xx3 > 1024 {
+			xx4 = float64(xx1) / 1024 //Gb
+		}
+
+		//xx3 = float64(xx2) / 1024 //Mb
+		//xx4 = float64(xx3) / 1024 //Gb
+
+		info += fmt.Sprintf("HugePage %.2f Gb : %d\n", xx4, amount)
 	}
 
 	for _, m := range memInfo.Modules {
 		fmt.Printf("%+v\n", m)
-	}
+	} //*ไม่ขึ้น
 
-	b, _ := json.MarshalIndent(memInfo.Modules, "", "  ")
-	fmt.Println(string(b))
+	c, _ := json.MarshalIndent(memInfo.Modules, "", "  ")
+	fmt.Println(string(c)) //*ไม่ขึ้น
 
 	memName := fmt.Sprintf("1 memName : %s", &memInfo.Area)
 	//c := memInfo.Area
